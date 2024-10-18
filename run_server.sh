64029fc361dc44656b62730eed26de8ddc875e94
Napat Wattanaputtakorn <dv22nwn@cs.umu.se>
2024-10-18 15:04:32 +0200

    Changed shell script

--- run_server.sh
+++ run_server.sh
@@ -2,5 +2,12 @@
 # Compile the program
 mvn compile
 
-# Run the server
-mvn exec:java -Dexec.mainClass="org.example.Main"
+read -p "Do you want to use a specific port: (y/n)" input
+
+if [[ "$input" == "Y" || "$input" == "y" ]]; then
+  read -p "Enter port number: " PORT
+  mvn exec:java -Dexec.mainClass="org.example.Main" -Dexec.args="-p $PORT"
+else
+  # Run the server with default port
+  mvn exec:java -Dexec.mainClass="org.example.Main"
+fi
